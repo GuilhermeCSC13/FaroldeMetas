ff752b38990cafccfdbc2550ff8a969df9856401
GuilhermeCSC13 <guilhermemaximocsc@gmail.com>
2025-12-26 22:08:18 -0300

    Update App.jsx

--- web/src/App.jsx
+++ web/src/App.jsx
@@ -1,18 +1,16 @@
 import { Routes, Route } from "react-router-dom";
 import Layout from "./components/Layout";
-import Dashboard from "./pages/Dashboard";
-import Reunioes from "./pages/Reunioes";
-import DetalheReuniao from "./pages/DetalheReuniao";
-import Configuracoes from "./pages/Configuracoes";
+import Inicio from "./pages/Inicio";
+import PlanejamentoTatico from "./pages/PlanejamentoTatico";
+import ReunioesPeriodicas from "./pages/ReunioesPeriodicas";
 
 export default function App() {
   return (
     <Layout>
       <Routes>
-        <Route path="/" element={<Dashboard />} />
-        <Route path="/reunioes" element={<Reunioes />} />
-        <Route path="/reunioes/:id" element={<DetalheReuniao />} />
-        <Route path="/config" element={<Configuracoes />} />
+        <Route path="/" element={<Inicio />} />
+        <Route path="/planejamento-tatico" element={<PlanejamentoTatico />} />
+        <Route path="/reunioes-periodicas" element={<ReunioesPeriodicas />} />
       </Routes>
     </Layout>
   );
