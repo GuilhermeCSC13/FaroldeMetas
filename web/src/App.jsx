66c58dcfc640c7fdea831d8589d17f93dce9f6a2
GuilhermeCSC13 <guilhermemaximocsc@gmail.com>
2025-12-27 13:40:00 -0300

    Update App.jsx

--- web/src/App.jsx
+++ web/src/App.jsx
@@ -1,17 +1,36 @@
-import { Routes, Route } from "react-router-dom";
-import Layout from "./components/Layout";
-import Inicio from "./pages/Inicio";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import Sidebar from "./components/Sidebar";
+
+import Home from "./pages/Home";
 import PlanejamentoTatico from "./pages/PlanejamentoTatico";
-import ReunioesPeriodicas from "./pages/ReunioesPeriodicas";
+import Operacao from "./pages/Operacao";
 
 export default function App() {
   return (
-    <Layout>
-      <Routes>
-        <Route path="/" element={<Inicio />} />
-        <Route path="/planejamento-tatico" element={<PlanejamentoTatico />} />
-        <Route path="/reunioes-periodicas" element={<ReunioesPeriodicas />} />
-      </Routes>
-    </Layout>
+    <BrowserRouter>
+      <div className="flex">
+        <Sidebar />
+
+        <main className="ml-64 p-6 w-full min-h-screen bg-slate-50">
+          <Routes>
+
+            <Route path="/" element={<Home />} />
+
+            <Route
+              path="/planejamento-tatico"
+              element={<PlanejamentoTatico />}
+            />
+
+            {/* NOVA ROTA */}
+            <Route
+              path="/operacao"
+              element={<Operacao />}
+            />
+
+          </Routes>
+        </main>
+      </div>
+    </BrowserRouter>
   );
 }
