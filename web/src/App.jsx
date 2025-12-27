8b743ed6fe72846f340ae3984eb0e1aaf617868f
GuilhermeCSC13 <guilhermemaximocsc@gmail.com>
2025-12-27 20:05:42 -0300

    Update App.jsx

--- web/src/App.jsx
+++ web/src/App.jsx
@@ -2,8 +2,12 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 // Páginas
 import Inicio from "./pages/Inicio";
-// O componente Operacao agora age como o "Controlador" (Abas: Resumo, Metas, Rotinas)
-import Operacao from "./pages/Operacao"; 
+
+// Módulos Táticos
+import Operacao from "./pages/Operacao"; // Controlador da Operação
+import Moov from "./pages/Moov";         // <--- Novo Controlador da Moov
+
+// Outras Páginas
 import ReunioesPeriodicas from "./pages/ReunioesPeriodicas";
 import DetalheReuniao from "./pages/DetalheReuniao";
 import Configuracoes from "./pages/Configuracoes";
@@ -12,21 +16,25 @@ export default function App() {
   return (
     <BrowserRouter>
       {/* Removemos o <Layout> global daqui. 
-        Agora cada módulo (ex: Operacao) chama seu próprio Layout (tatico/Layout).
+         Cada módulo (Operacao, Moov, etc) chama seu próprio Layout (tatico/Layout).
       */}
       <Routes>
         <Route path="/" element={<Inicio />} />
         
-        {/* Rota principal do Módulo Operação */}
-        {/* Este componente gerencia os hashs #metas, #rotinas, #resumo */}
+        {/* --- MÓDULO OPERAÇÃO --- */}
+        {/* Gerencia #metas, #rotinas, #resumo */}
         <Route path="/planejamento/operacao" element={<Operacao />} />
 
-        {/* Rotas secundárias / Placeholders */}
+        {/* --- MÓDULO MOOV (Novo) --- */}
+        {/* Gerencia #metas, #rotinas */}
+        <Route path="/moov" element={<Moov />} />
+
+        {/* --- ROTAS SECUNDÁRIAS --- */}
         <Route path="/reunioes-periodicas" element={<ReunioesPeriodicas />} />
         <Route path="/reunioes/:id" element={<DetalheReuniao />} />
         <Route path="/configuracoes" element={<Configuracoes />} />
         
-        {/* Redirecionamentos ou rotas futuras */}
+        {/* Redirecionamentos */}
         <Route path="/planejamento-tatico" element={<Inicio />} /> 
       </Routes>
     </BrowserRouter>
