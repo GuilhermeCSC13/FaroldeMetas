ec23c0a2f9da7e98c86fde51e65cddea2b22d95f
GuilhermeCSC13 <guilhermemaximocsc@gmail.com>
2025-12-27 13:43:33 -0300

    Update App.jsx

--- web/src/App.jsx
+++ web/src/App.jsx
@@ -1,36 +1,61 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+// src/App.jsx
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
-import Sidebar from "./components/Sidebar";
+import Sidebar from "./components/tatico/Sidebar";
 
-import Home from "./pages/Home";
+// Páginas
+import Inicio from "./pages/Inicio";
 import PlanejamentoTatico from "./pages/PlanejamentoTatico";
 import Operacao from "./pages/Operacao";
+import ReunioesPeriodicas from "./pages/ReunioesPeriodicas";
+import DetalheReuniao from "./pages/DetalheReuniao";
+import Dashboard from "./pages/Dashboard";
+import Configuracoes from "./pages/Configuracoes";
 
-export default function App() {
+function App() {
   return (
-    <BrowserRouter>
-      <div className="flex">
+    <Router>
+      <div className="flex min-h-screen bg-gray-100">
+        {/* Sidebar fixa à esquerda */}
         <Sidebar />
 
-        <main className="ml-64 p-6 w-full min-h-screen bg-slate-50">
+        {/* Conteúdo principal */}
+        <main className="flex-1 p-4 md:p-6 overflow-y-auto">
           <Routes>
+            {/* Início */}
+            <Route path="/" element={<Inicio />} />
 
-            <Route path="/" element={<Home />} />
-
+            {/* Planejamento Tático (visão geral dos setores) */}
             <Route
               path="/planejamento-tatico"
               element={<PlanejamentoTatico />}
             />
 
-            {/* NOVA ROTA */}
+            {/* Planejamento / Farol da Operação */}
+            <Route path="/operacao" element={<Operacao />} />
+
+            {/* Reuniões Periódicas */}
             <Route
-              path="/operacao"
-              element={<Operacao />}
+              path="/reunioes-periodicas"
+              element={<ReunioesPeriodicas />}
             />
 
+            {/* Detalhe de uma reunião específica */}
+            <Route
+              path="/reunioes-periodicas/:id"
+              element={<DetalheReuniao />}
+            />
+
+            {/* Dashboard (caso esteja usando como visão sintética) */}
+            <Route path="/dashboard" element={<Dashboard />} />
+
+            {/* Configurações gerais da ferramenta */}
+            <Route path="/configuracoes" element={<Configuracoes />} />
           </Routes>
         </main>
       </div>
-    </BrowserRouter>
+    </Router>
   );
 }
+
+export default App;
