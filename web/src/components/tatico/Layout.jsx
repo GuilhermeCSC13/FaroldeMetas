44715ebbfd95c3cb543422665d67fb91f9a21e86
GuilhermeCSC13 <guilhermemaximocsc@gmail.com>
2025-12-28 08:05:12 -0300

    Update Layout.jsx

--- web/src/components/tatico/Layout.jsx
+++ web/src/components/tatico/Layout.jsx
@@ -1,22 +1,19 @@
-import React from 'react';
-import Sidebar from './Sidebar';
+import Sidebar from "./Sidebar";
+import TacticalAssistant from "./TacticalAssistant"; // <--- Importe aqui
 
-const Layout = ({ children }) => {
+export default function Layout({ children }) {
   return (
-    <div className="flex min-h-screen bg-gray-50">
-      {/* Sidebar Fixa */}
-      <div className="fixed inset-y-0 left-0 z-50">
-        <Sidebar />
-      </div>
-
-      {/* Área de Conteúdo (Deslocada para a direita) */}
-      <main className="flex-1 pl-64 transition-all duration-300">
-        <div className="p-8">
-          {children}
+    <div className="flex min-h-screen bg-slate-50">
+      <Sidebar />
+      <main className="flex-1 flex flex-col h-screen overflow-hidden relative">
+        {/* Conteúdo da Página */}
+        <div className="flex-1 overflow-auto">
+           {children}
         </div>
+        
+        {/* Assistente Flutuante (Fica por cima de tudo) */}
+        <TacticalAssistant /> 
       </main>
     </div>
   );
-};
-
-export default Layout;
+}
