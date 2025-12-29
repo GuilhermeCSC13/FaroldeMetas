a3786966ebbac86666fca78d719f2c2df660fcba
GuilhermeCSC13 <guilhermemaximocsc@gmail.com>
2025-12-29 10:53:01 -0300

    Update App.jsx

--- web/src/App.jsx
+++ web/src/App.jsx
@@ -7,6 +7,8 @@ import Inicio from "./pages/Inicio";
 import Operacao from "./pages/Operacao";
 import Moov from "./pages/Moov";
 import Manutencao from "./pages/Manutencao";
+import Financeiro from "./pages/Financeiro"; // NOVO
+import Pessoas from "./pages/Pessoas";       // NOVO
 
 // Reuniões & Ações
 import ReunioesCalendario from "./pages/ReunioesCalendario";
@@ -15,7 +17,7 @@ import GestaoAcoes from "./pages/GestaoAcoes";
 
 // Novos Módulos de Gestão
 import CentralReunioes from "./pages/CentralReunioes";
-import CentralAtas from "./pages/CentralAtas"; // <--- NOVO IMPORT
+import CentralAtas from "./pages/CentralAtas";
 
 // Inteligência Artificial
 import Copiloto from "./pages/Copiloto";
@@ -27,31 +29,26 @@ export default function App() {
   return (
     <BrowserRouter>
       <Routes>
-        {/* Home */}
         <Route path="/" element={<Inicio />} />
         
         {/* --- MÓDULOS DE ÁREAS --- */}
         <Route path="/planejamento/operacao" element={<Operacao />} />
+        <Route path="/planejamento/financeiro" element={<Financeiro />} />
+        <Route path="/planejamento/pessoas" element={<Pessoas />} />
         <Route path="/moov" element={<Moov />} />
         <Route path="/manutencao" element={<Manutencao />} />
 
         {/* --- MÓDULO REUNIÕES & ATAS --- */}
-        <Route path="/central-reunioes" element={<CentralReunioes />} /> {/* Agenda Tática */}
-        <Route path="/central-atas" element={<CentralAtas />} />         {/* Banco de Atas (NOVO) */}
-        
+        <Route path="/central-reunioes" element={<CentralReunioes />} />
+        <Route path="/central-atas" element={<CentralAtas />} />
         <Route path="/gestao-acoes" element={<GestaoAcoes />} />
 
-        {/* Rotas Legado/Específicas */}
+        {/* Rotas Legado */}
         <Route path="/reunioes-calendario" element={<ReunioesCalendario />} />
         <Route path="/reunioes/:id" element={<DetalheReuniao />} />
 
-        {/* --- COPILOTO IA --- */}
         <Route path="/copiloto" element={<Copiloto />} />
-
-        {/* --- CONFIGURAÇÕES GERAIS --- */}
         <Route path="/configuracoes" element={<Configuracoes />} />
-        
-        {/* Redirecionamento de segurança */}
         <Route path="/planejamento-tatico" element={<Inicio />} /> 
       </Routes>
     </BrowserRouter>
