495bcc59747a2287b7160c03ddc382a0b198688e
GuilhermeCSC13 <guilhermemaximocsc@gmail.com>
2026-01-29 13:19:54 -0300

    Update Layout.jsx

--- web/src/components/tatico/Layout.jsx
+++ web/src/components/tatico/Layout.jsx
@@ -1,5 +1,7 @@
+// src/components/tatico/Layout.jsx
 import Sidebar from "./Sidebar";
-import TacticalAssistant from "./TacticalAssistant"; // <--- Importe aqui
+import TacticalAssistant from "./TacticalAssistant";
+import RecorderDock from "./RecorderDock"; // ✅ NOVO (dock do gravador)
 
 export default function Layout({ children }) {
   return (
@@ -7,12 +9,13 @@ export default function Layout({ children }) {
       <Sidebar />
       <main className="flex-1 flex flex-col h-screen overflow-hidden relative">
         {/* Conteúdo da Página */}
-        <div className="flex-1 overflow-auto">
-           {children}
-        </div>
-        
-        {/* Assistente Flutuante (Fica por cima de tudo) */}
-        <TacticalAssistant /> 
+        <div className="flex-1 overflow-auto">{children}</div>
+
+        {/* Dock de Gravação (global, sempre disponível) */}
+        <RecorderDock />
+
+        {/* Assistente Flutuante (fica por cima de tudo) */}
+        <TacticalAssistant />
       </main>
     </div>
   );
